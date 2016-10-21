86eb5794da1863aa5438febb73d4e254eee05287
Ted Moke <theodore.moke@gmail.com>
2016-10-21 15:47:21 -0400

    Prettified html/css/js

--- scripts.js
+++ scripts.js
@@ -10,49 +10,48 @@
 
 
 //Contact Form
-$("#contactForm").validator().on("submit", function (event) {
-	if (event.isDefaultPrevented()) {
-		submitMSG(false, "Did you fill in the form properly?");
-	}
-	else{
-		event.preventDefault();
-		submitForm();
-	}
+$("#contactForm").validator().on("submit", function(event) {
+    if (event.isDefaultPrevented()) {
+        submitMSG(false, "Did you fill in the form properly?");
+    } else {
+        event.preventDefault();
+        submitForm();
+    }
 });
 
-function submitForm(){
-	//Create variables with contact form content
-	var name = $("#name").val();
-	var email = $("#email").val();
-	var inquiry = $("#inquiry").val();
-	var phone = $("#phone").val();
+function submitForm() {
+    //Create variables with contact form content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var inquiry = $("#inquiry").val();
+    var phone = $("#phone").val();
 
-	$.ajax({
-		type: "POST",
-		url: "contact_form/php/process.php",
-		data: "name=" + name + "&email=" + email + "&phone=" + phone + "&inquiry=" + inquiry,
-		success : function(text){
-			if (text == "success"){
-				formSuccess();
-			} else {
-				//formError(); - only if animate is implemented
-				submitMSG(false, text);
-			}
-		}
-	})
+    $.ajax({
+        type: "POST",
+        url: "contact_form/php/process.php",
+        data: "name=" + name + "&email=" + email + "&phone=" + phone + "&inquiry=" + inquiry,
+        success: function(text) {
+            if (text == "success") {
+                formSuccess();
+            } else {
+                //formError(); - only if animate is implemented
+                submitMSG(false, text);
+            }
+        }
+    })
 }
 
-function formSuccess(){
-	$("#contactForm")[0].reset();
-	submitMSG(true, "Inquiry Submitted!")
+function formSuccess() {
+    $("#contactForm")[0].reset();
+    submitMSG(true, "Inquiry Submitted!")
 }
 
-function submitMSG(valid, msg){
-	var msgClasses;
-	if(valid){
-		msgClasses = "h3 text-center msg-success col-xs-12";
-	} else {
-		msgClasses = "h3 text-center msg-error col-xs-12";
-	}
-	$("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+function submitMSG(valid, msg) {
+    var msgClasses;
+    if (valid) {
+        msgClasses = "h3 text-center msg-success col-xs-12";
+    } else {
+        msgClasses = "h3 text-center msg-error col-xs-12";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
 }
